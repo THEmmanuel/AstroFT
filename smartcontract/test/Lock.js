410fe55d611c207cba62376a45136721208188cb
malawadd <50057882+malawadd@users.noreply.github.com>
2022-08-04 21:53:06 +0300

    init hardhat project

---
+++ smartcontract/test/Lock.js
@@ -0,0 +1,15 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+
+describe("AstrFt", function () {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshopt in every test.
+  
+  }
+
+);
