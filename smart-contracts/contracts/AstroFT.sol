c431916d4b4f275f508cc2355f7c228ea37fe98f
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-08-05 02:25:53 +0100

    add openzepellin contracts

--- smart-contracts/contracts/AstroFT.sol
+++ smart-contracts/contracts/AstroFT.sol
@@ -3,5 +3,9 @@ pragma solidity ^0.8.9;
 
 // Import this file to use console.log
 import "hardhat/console.sol";
+// Import from openzepellin erc 721 contract.
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract AstroFT {}
+contract AstroFT is ERC721 {
+
+}
